6b570d327b8d6a8203963c48c6e794bb6e922909
runquan-ray-zhou <151119924+runquan-ray-zhou@users.noreply.github.com>
2024-06-14 19:25:33 -0400

    add run function in index.js

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 console.log("Welcome to my app!");
+
+const { writeJSONFile, readJSONFile } = require("./src/helpers")
+
+const inform = console.log
+
+function run() {
+    const action = process.argv[2];
+    const productName = process.argv[3];
+    switch (action) {
+      case "index":
+        inform(action);
+        break;
+      case "create":
+        inform(action, productName);
+        break;
+      case "show":
+        inform(action, productName);
+        break;
+      case "update":
+        inform(action, productName);
+        break;
+      case "destroy":
+        inform(action, productName);
+        break;
+      case "score":
+        inform(action);
+        break;
+      default:
+        inform("There was an error.");
+    }
+  }
+  run();
