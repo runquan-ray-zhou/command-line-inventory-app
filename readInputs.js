f2c9bc90e84300c2c63d67b16f510407f3fa1014
runquan-ray-zhou <151119924+runquan-ray-zhou@users.noreply.github.com>
2024-06-15 00:09:27 -0400

    update add functionality

--- readInputs.js
+++ readInputs.js
@@ -18,11 +18,29 @@ rl.close()
 
 function createProduct() {
     rl.question('\nWhat is the product name?\n\n', (productName) => {
+        rl.question('\nWhat is the product code?\n\n', (productCode) => {
+            rl.question('\nWhat is the product type?\n\n', (productType) => {
+                rl.question('\nWhat is the product brand?\n\n', (productBrand) => {
+                    rl.question('\nWho is the product vender?\n\n', (productVendor) => {
+                        rl.question('\nWhat is the product FDA status?\n\n', (productFDAStatus) => {
+                            rl.question('\nWhat is the product count?\n\n', (productCaseCount) => {
+                                rl.question('\nWhat is the product lb per case?\n\n', (productLbPerCase) => {
+                                    rl.question('\nWhat is the product price per lb?\n\n', (productCostPerLb) => {
+                                        rl.question('\nWhere is the product pallet location?\n\n', (productLocation) => {
+                                            rl.question('\nWho is the product purchaser/buyer?\n\n', (productBuyer) => {
+                                                rl.question('\nWhen was the product purchased?\n\n', (productPurchaseDate) => {
+                                                    rl.question('\nWhen was the product entry date?\n\n', (productEnterDate) => {
+                                                        rl.question('\nWhen does product expire?\n\n', (productExpireDate) => {
     let products = readJSONFile("./data", "products.json")
     let writeToFile = false;
     let updateProducts = []
 
-    updateProducts = create(products, productName)
+    const count = parseInt(productCaseCount)
+    const pound = parseInt(productLbPerCase)
+    const cost = parseInt(productCostPerLb)
+
+    updateProducts = create(products, productName, productCode, productType, productBrand, productVendor, productFDAStatus, count, pound, cost, productLocation, productBuyer, productPurchaseDate, productEnterDate, productExpireDate)
+
     writeToFile = true;
 
     if (writeToFile) {
@@ -30,7 +48,20 @@ function createProduct() {
     }
 
     rl.close()
-})
+                                                        })
+                                                    })
+                                                })
+                                            })
+                                        })
+                                    })
+                                })  
+                            })
+                        })
+                    })  
+                })
+            })
+        })
+    })
 }
 
 function removeProduct() {
@@ -51,12 +82,12 @@ function removeProduct() {
 }
 
 function getCommand() {
-    rl.question('HAL 9001: What is your command?\n\nPlease choose from the following:\n\nDisplay a list of products: Type - index\nAdd product to inventory:   Type - add\nDisplay product details:    Type - show\nRemove/delete product:      Type - delete\nUpdate product detail:      Type - update\nDisplay total amount:       Type - calculate\n\n', (command) => {
+    rl.question('HAL 9001: Hello, what is your command?\n\nPlease choose from the following:\n\nDisplay a list of products: Type - index\nAdd product to inventory:   Type - add\nDisplay product details:    Type - show\nRemove/delete product:      Type - remove\nUpdate product detail:      Type - update\nDisplay total amount:       Type - calculate\n\n', (command) => {
     switch (command) {
         case "index":
         getIndex();
         break;
-        case "create":
+        case "add":
         createProduct()
         break;
         case "show":
